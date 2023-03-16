0b169d6634358710b4bbf116d9f07ebd09073b49
TereseBo~ <terese.boderus@yh.nackademin.se>
2023-03-16 11:57:41 +0100

    Fixed done/undone msg and functions

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -18,7 +18,13 @@ export default function Item({ item, setMsg, setStatus, setList, list, parentlis
         const data = await res.text()
         switch (res.status) {
             case 200:
-                let res2 = await fetch(`http://localhost:5050/content/item?id=${e.target.id}`, { credentials: 'include' })
+                let bob= await updateItem(e.target.id)
+                console.log(bob)
+                if(bob){
+                //updateItem(e.target.id)
+                  setPgMsg('Item marked as done')
+                }
+/*                 let res2 = await fetch(`http://localhost:5050/content/item?id=${e.target.id}`, { credentials: 'include' })
                 const data = await res2.text()
                 if (res2.status === 200) {
                     let returneditems = JSON.parse(data)
@@ -38,8 +44,8 @@ export default function Item({ item, setMsg, setStatus, setList, list, parentlis
                     });
                     setList(listCopy)
                 }
-                setMsg('Item marked as done')
-                break;
+                setPgMsg('Item marked as done')
+ */                break;
             case 401:
                 setMsg(data)
                 setStatus(false)
@@ -61,8 +67,13 @@ export default function Item({ item, setMsg, setStatus, setList, list, parentlis
         switch (res.status) {
 
             case 200:
-                updateItem(e.target.id)
-            break;
+                let bob= await updateItem(e.target.id)
+                console.log(bob)
+                if(bob){
+                //updateItem(e.target.id)
+                  setPgMsg('Item marked as undone')
+                }
+                break;
             case 401:
                 setMsg(data)
                 setPgMsg('  ')
@@ -75,99 +86,114 @@ export default function Item({ item, setMsg, setStatus, setList, list, parentlis
     async function updateItem(itemId) {
         let res2 = await fetch(`http://localhost:5050/content/item?id=${itemId}`, { credentials: 'include' })
         const data2 = await res2.text()
-        if (res2.status === 200) {
-            let returneditems = JSON.parse(data2)
-            let listCopy = list
-            listCopy.forEach(list => {
-                if (list.id === parentlist) {
-                    list.items.forEach(item => {
-                        if (item.id == itemId) {
-                            console.log('found')
-                            console.log(item)
-                            console.log(item.id)
-                            item.completed = returneditems[0].completed
-                            console.log(item)
-                        }
-                    });
-                }
-            });
-            console.log(listCopy)
-            setList(listCopy)
-        }
-        setMsg('Item marked as undone')
-    
-    }
-
-    async function deleteItem(e) {
-
-        let res = await fetch(`http://localhost:5050/content/item/`, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ id: e.target.id }),
-            credentials: 'include'
-        })
-        const data = await res.text()
-        switch (res.status) {
-            case 200:
-                refetchItems(parentlist)
-                setPgMsg('Item deleted')
-                break;
-            case 401:
-                setMsg(data)
-                setPgMsg('  ')
-                setStatus(false)
-                break;
-            default:
-                setMsg(data)
-        }
-    }
-
-    async function refetchItems(listId) {
-
-        let res2 = await fetch(`http://localhost:5050/content/list/?id=${listId}`, { credentials: 'include' })
-        const data = await res2.text()
-        let listCopy = list//[...list]
+        let success = false
         switch (res2.status) {
             case 200:
-                listCopy.find((list) => list.id == listId).items = JSON.parse(data)
+                let returneditems = JSON.parse(data2)
+                let listCopy = list
+                listCopy.forEach(list => {
+                    if (list.id === parentlist) {
+                        list.items.forEach(item => {
+                            if (item.id == itemId) {
+                                console.log('found')
+                                console.log(item)
+                                console.log(item.id)
+                                item.completed = returneditems[0].completed
+                                console.log(item)
+                            }
+                        });
+                    }
+                });
+                console.log(listCopy)
                 setList(listCopy)
-                setMsg(`Items loaded for list ${listCopy.find((list) => list.id == listId).name}`)
-                break;
-            case 204:
-                setPgMsg(`No items left in ${listCopy.find((list) => list.id == listId).name}`)
-                delete listCopy.find((list) => list.id == listId).items
-                setList(listCopy)
+                 success = true
                 break;
             case 401:
-                setMsg(data)
+                setMsg(data2)
                 setPgMsg('  ')
                 setStatus(false)
                 break;
             default:
-                setPgMsg(data)
-                break
+                setPgMsg(data2)
+
         }
+        return success
     }
 
 
-    return (
-        <div className="item" id={parentlist}>
-            <h3>{item.name}</h3>
-            <p>{item.description}</p>
-            <p><span>created: </span>{item.created}</p>
-            {item.completed !== null ?
+
+
+async function deleteItem(e) {
+
+    let res = await fetch(`http://localhost:5050/content/item/`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ id: e.target.id }),
+        credentials: 'include'
+    })
+    const data = await res.text()
+    switch (res.status) {
+        case 200:
+            refetchItems(parentlist)
+            setPgMsg('Item deleted')
+            break;
+        case 401:
+            setMsg(data)
+            setPgMsg('  ')
+            setStatus(false)
+            break;
+        default:
+            setMsg(data)
+    }
+}
+
+async function refetchItems(listId) {
+
+    let res2 = await fetch(`http://localhost:5050/content/list/?id=${listId}`, { credentials: 'include' })
+    const data = await res2.text()
+    let listCopy = list//[...list]
+    switch (res2.status) {
+        case 200:
+            //setPgMsg('')
+            listCopy.find((list) => list.id == listId).items = JSON.parse(data)
+            setList(listCopy)
+            setMsg(`Items loaded for list ${listCopy.find((list) => list.id == listId).name}`)
+            break;
+        case 204:
+            setPgMsg(`No items left in ${listCopy.find((list) => list.id == listId).name}`)
+            delete listCopy.find((list) => list.id == listId).items
+            setList(listCopy)
+            break;
+        case 401:
+            setMsg(data)
+            setPgMsg('  ')
+            setStatus(false)
+            break;
+        default:
+            setPgMsg(data)
+            break
+    }
+}
+
+
+return (
+    <div className="item" id={parentlist}>
+        <h3>{item.name}</h3>
+        <p>{item.description}</p>
+        <p><span>created: </span>{item.created}</p>
+        {item.completed !== null ?
             (<p><span>completed: </span>{item.completed} </p>)
-            :(<p>Not done</p>) }
-            <div className="button-container">
-            {item.completed === null ? 
-            (<Addbutton id={item.id} text="Mark as done" callback={markAsDone} />) 
-            : 
-            ( <Addbutton id={item.id} text="Unmark as done" callback={unmarkAsDone} />)
+            : (<p>Not done</p>)}
+        <div className="button-container">
+            {item.completed === null ?
+                (<Addbutton id={item.id} text="Mark as done" callback={markAsDone} />)
+                :
+                (<Addbutton id={item.id} text="Unmark as done" callback={unmarkAsDone} />)
             }
             <Addbutton id={item.id} text="Delete" callback={deleteItem} />
-            </div>
         </div>
-    )
+    </div>
+)
 }
