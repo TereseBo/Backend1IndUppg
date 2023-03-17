3a046a1b237b61c0daee23f536bf78e8e623a22f
TereseBo~ <terese.boderus@yh.nackademin.se>
2023-03-17 15:54:39 +0100

    Added validation of cookie in check cookie

--- server/middlewares/checkCookie.js
+++ server/middlewares/checkCookie.js
@@ -1,14 +1,31 @@
 const jwt = require('jsonwebtoken');
+const joi = require('joi')
+
+const itemSchema = joi.object({
+    id: joi.number().integer().required().min(1),
+    name: joi.string().min(3).max(50),
+    iat: joi.number().integer().required().min(1),
+    exp: joi.number().integer().required().min(1)
+
+})
 
 function checkCookie(req, res, next) {
 
     try {
         const token = req.cookies.authToken;
         const decoded = jwt.verify(token, process.env.SECRET_KEY);
+        console.log(decoded)
+        const { error, value } = itemSchema.validate(decoded)
+        if (error) {
+            res.status(400).send(error.details[0].message)
+            return
+        }
+        const { id, name } = value
         req.user = {
-            id: decoded.id,
-            name: decoded.name
+            id: id,
+            name: name
         };
+
         next();
     } catch (err) {
         res.status(401).send('You are not authorized to view this page, please login');
